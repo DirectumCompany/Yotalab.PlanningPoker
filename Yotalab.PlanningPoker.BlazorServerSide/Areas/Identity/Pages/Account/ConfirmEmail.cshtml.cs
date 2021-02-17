71d96ac9ec1901dd00fd1c8ecf6acabcbd25c9cb
Киршин Максим <hankir@live.ru>
2021-02-17 22:50:39 +0400

    Добавлен статус результата подтверждения эл. почты и логгирование в случае ошибки.

--- Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,25 +1,31 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
 
 namespace Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity.Pages.Account
 {
   [AllowAnonymous]
   public class ConfirmEmailModel : PageModel
   {
-    private readonly UserManager<IdentityUser> _userManager;
+    private readonly UserManager<IdentityUser> userManager;
+    private readonly ILogger<ConfirmEmailModel> logger;
 
-    public ConfirmEmailModel(UserManager<IdentityUser> userManager)
+    public ConfirmEmailModel(UserManager<IdentityUser> userManager, ILogger<ConfirmEmailModel> logger)
     {
-      _userManager = userManager;
+      this.userManager = userManager;
+      this.logger = logger;
     }
 
     [TempData]
     public string StatusMessage { get; set; }
+
+    public IdentityResult Result { get; set; }
 
     public string ReturnUrl { get; set; }
 
@@ -30,16 +36,20 @@ namespace Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity.Pages.Account
         return RedirectToPage("/");
       }
 
-      var user = await _userManager.FindByIdAsync(userId);
+      var user = await userManager.FindByIdAsync(userId);
       if (user == null)
       {
         return NotFound($"Unable to load user with ID '{userId}'.");
       }
 
       code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-      var result = await _userManager.ConfirmEmailAsync(user, code);
-      StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+      this.Result = await userManager.ConfirmEmailAsync(user, code);
+      this.StatusMessage = this.Result.Succeeded ? "Спасибо, что выбрали нас!" : "Ошибка подтверждения эл. почты.";
       this.ReturnUrl = returnUrl;
+      if (!this.Result.Succeeded)
+      {
+        this.logger.LogWarning("Confirm email ({Email}) failed: {Error}", user.Email, string.Join(";", this.Result.Errors.Select(e => e.Description)));
+      }
       return Page();
     }
   }
