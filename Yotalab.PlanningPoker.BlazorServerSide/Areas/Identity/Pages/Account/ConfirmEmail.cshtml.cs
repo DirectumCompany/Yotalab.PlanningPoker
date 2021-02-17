526e58319576243f7feb56faa2aa342cf9074db0
Киршин Максим <hankir@live.ru>
2021-02-17 22:25:29 +0400

    Добавлен проброс ReturnUrl, чтобы при разлогиненной сессии и при переходе по ссылке на сессию, после входа открывалась сразу карточка сессии. А так же после регистрации.

--- Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -21,11 +21,13 @@ namespace Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity.Pages.Account
     [TempData]
     public string StatusMessage { get; set; }
 
-    public async Task<IActionResult> OnGetAsync(string userId, string code)
+    public string ReturnUrl { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(string userId, string code, string returnUrl)
     {
       if (userId == null || code == null)
       {
-        return this.Redirect("/");
+        return RedirectToPage("/");
       }
 
       var user = await _userManager.FindByIdAsync(userId);
@@ -37,6 +39,7 @@ namespace Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity.Pages.Account
       code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
       var result = await _userManager.ConfirmEmailAsync(user, code);
       StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+      this.ReturnUrl = returnUrl;
       return Page();
     }
   }
