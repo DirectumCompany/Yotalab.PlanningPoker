53c92cb0dcd04be8ff14277e2e446084115d631a
Vladimir Zhivotov <vladimir.2@mail.ru>
2021-02-28 15:09:42 +0400

    Лишний пробел.

--- Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/IdentityHostingStartup.cs
+++ Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/IdentityHostingStartup.cs
@@ -18,7 +18,6 @@ namespace Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity
 {
   public class IdentityHostingStartup : IHostingStartup
   {
-
     public void Configure(IWebHostBuilder builder)
     {
       builder.ConfigureServices((context, services) =>
