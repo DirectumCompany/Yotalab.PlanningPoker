392cd872b19ec00a187bdee249247736b3d9cc85
LDC7 <lihwar1@gmail.com>
2021-03-11 22:51:49 +0400

    Комментарии?

--- Yotalab.PlanningPoker.BlazorServerSide/Services/Args/EditSessionArgs.cs
+++ Yotalab.PlanningPoker.BlazorServerSide/Services/Args/EditSessionArgs.cs
@@ -2,10 +2,19 @@
 
 namespace Yotalab.PlanningPoker.BlazorServerSide.Services.Args
 {
+  /// <summary>
+  /// Аргументы события редактирования сессии планирования.
+  /// </summary>
   public class EditSessionArgs
   {
+    /// <summary>
+    /// Идентификатор сессии планирования.
+    /// </summary>
     public Guid SessionId { get; set; }
 
+    /// <summary>
+    /// Имя сессии.
+    /// </summary>
     public string Name { get; set; }
   }
 }
--- Yotalab.PlanningPoker.Grains.Interfaces/Models/Args/ChangeSessionInfoArgs.cs
+++ Yotalab.PlanningPoker.Grains.Interfaces/Models/Args/ChangeSessionInfoArgs.cs
@@ -1,7 +1,13 @@
 ﻿namespace Yotalab.PlanningPoker.Grains.Interfaces.Models.Args
 {
+  /// <summary>
+  /// Аргументы изменения информации о сессии.
+  /// </summary>
   public class ChangeSessionInfoArgs
   {
+    /// <summary>
+    /// Имя сессии.
+    /// </summary>
     public string Name { get; set; }
   }
 }
