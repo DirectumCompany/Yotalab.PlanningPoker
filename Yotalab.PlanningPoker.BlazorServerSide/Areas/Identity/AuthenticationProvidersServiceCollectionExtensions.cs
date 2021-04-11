ae5b8c76c2de3ef46ba1d4016ea61fd930ec9221
Киршин Максим <hankir@live.ru>
2021-04-11 15:02:38 +0400

    Добавлены проверки на возможность сконфигурировать вход через Google/Microsoft учетную запись, чтобы без указания секретов приложение запускалось.

---
+++ Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/AuthenticationProvidersServiceCollectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity
+{
+  /// <summary>
+  /// Расширения для настройки входа через сторонние провайдеры аутентификации.
+  /// </summary>
+  public static class AuthenticationProvidersServiceCollectionExtensions
+  {
+    /// <summary>
+    /// Попробовать сконфигурировать вход через учетную запись Microsoft, при наличии в конфигурации приложения ClientId и ClientSecret.
+    /// </summary>
+    /// <param name="authencticationBuilder">Построитель аутентификации.</param>
+    /// <param name="configuration">Конфигурация приложения.</param>
+    /// <returns>Построитель аутентификации, сконфигурированный для входа через учетную запись Microsoft,
+    /// если в конфигурации приложения указаны необходимые ключи.</returns>
+    public static AuthenticationBuilder TryConfigureMicrosoftAccount(this AuthenticationBuilder authencticationBuilder, IConfiguration configuration)
+    {
+      var microsoftAccountClientId = configuration["Authentication:Microsoft:ClientId"];
+      var microsoftAccountClientSecret = configuration["Authentication:Microsoft:ClientSecret"];
+      if (string.IsNullOrWhiteSpace(microsoftAccountClientId) || string.IsNullOrWhiteSpace(microsoftAccountClientSecret))
+        return authencticationBuilder;
+
+      return authencticationBuilder.AddMicrosoftAccount(options =>
+      {
+        options.ClientId = microsoftAccountClientId;
+        options.ClientSecret = microsoftAccountClientSecret;
+      });
+    }
+
+    /// <summary>
+    /// Попробовать сконфигурировать вход через учетную запись Google, при наличии в конфигурации приложения ClientId и ClientSecret.
+    /// </summary>
+    /// <param name="authencticationBuilder">Построитель аутентификации.</param>
+    /// <param name="configuration">Конфигурация приложения.</param>
+    /// <returns>Построитель аутентификации, сконфигурированный для входа через учетную запись Google,
+    /// если в конфигурации приложения указаны необходимые ключи.</returns>
+    public static AuthenticationBuilder TryConfigureGoogleAccount(this AuthenticationBuilder authencticationBuilder, IConfiguration configuration)
+    {
+      var googleAccountClientId = configuration["Authentication:Google:ClientId"];
+      var googleAccountClientSecret = configuration["Authentication:Google:ClientSecret"];
+      if (string.IsNullOrWhiteSpace(googleAccountClientId) || string.IsNullOrWhiteSpace(googleAccountClientSecret))
+        return authencticationBuilder;
+
+      return authencticationBuilder.AddMicrosoftAccount(options =>
+      {
+        options.ClientId = googleAccountClientId;
+        options.ClientSecret = googleAccountClientSecret;
+        options.Events = new Microsoft.AspNetCore.Authentication.OAuth.OAuthEvents
+        {
+          OnCreatingTicket = c =>
+          {
+            var identity = (ClaimsIdentity)c.Principal.Identity;
+            var avatar = c.User.GetProperty("picture").GetString();
+            identity.AddClaim(new Claim("avatar", avatar));
+            return Task.CompletedTask;
+          }
+        };
+      });
+    }
+  }
+}
--- Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/IdentityHostingStartup.cs
+++ Yotalab.PlanningPoker.BlazorServerSide/Areas/Identity/IdentityHostingStartup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -6,10 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using System;
-using System.IO;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using Yotalab.PlanningPoker.BlazorServerSide.Data;
 
 [assembly: HostingStartup(typeof(Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity.IdentityHostingStartup))]
@@ -40,26 +38,8 @@ namespace Yotalab.PlanningPoker.BlazorServerSide.Areas.Identity
 
         services
           .AddAuthentication()
-          .AddMicrosoftAccount(options =>
-          {
-            options.ClientId = context.Configuration["Authentication:Microsoft:ClientId"];
-            options.ClientSecret = context.Configuration["Authentication:Microsoft:ClientSecret"];
-          })
-          .AddGoogle(options =>
-          {
-            options.ClientId = context.Configuration["Authentication:Google:ClientId"];
-            options.ClientSecret = context.Configuration["Authentication:Google:ClientSecret"];
-            options.Events = new Microsoft.AspNetCore.Authentication.OAuth.OAuthEvents
-            {
-              OnCreatingTicket = c =>
-              {
-                var identity = (ClaimsIdentity)c.Principal.Identity;
-                var avatar = c.User.GetProperty("picture").GetString();
-                identity.AddClaim(new Claim("avatar", avatar));
-                return Task.FromResult(0);
-              }
-            };
-          });
+          .TryConfigureMicrosoftAccount(context.Configuration)
+          .TryConfigureGoogleAccount(context.Configuration);
 
         services.ConfigureApplicationCookie(options =>
         {
