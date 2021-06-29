113189dbd10cc2631dceda620a31672fc10c81d0
Киршин Максим <hankir@live.ru>
2021-06-29 21:10:28 +0400

    Возвращен выпадающий список с поддержкой модели для создания элементов.

--- Yotalab.PlanningPoker.BlazorServerSide/Shared/DropDownMenu.razor.cs
+++ Yotalab.PlanningPoker.BlazorServerSide/Shared/DropDownMenu.razor.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Yotalab.PlanningPoker.BlazorServerSide.Shared
 {
   public class DropDownMenuItem
   {
     public string ItemContainerClassName { get; set; }
+
+    public Action<MouseEventArgs> OnClick { get; set; }
+
+    public Func<MouseEventArgs, Task> OnClickAsync { get; set; }
 
     public RenderFragment ItemTemplate { get; set; }
   }
