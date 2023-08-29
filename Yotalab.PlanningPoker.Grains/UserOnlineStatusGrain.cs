cc6601ce7ed9b2c0e32032b530a23ca73b37a626
Киршин Максим <hankir@live.ru>
2023-08-29 20:00:06 +0400

    Добавлено логгирование статуса пользователя в сети/не в сети.

--- Yotalab.PlanningPoker.Grains/UserOnlineStatusGrain.cs
+++ Yotalab.PlanningPoker.Grains/UserOnlineStatusGrain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Orleans;
 using Yotalab.PlanningPoker.Grains.Interfaces;
 using Yotalab.PlanningPoker.Grains.Interfaces.Models.Notifications;
@@ -9,6 +10,13 @@ namespace Yotalab.PlanningPoker.Grains
 {
   public class UserOnlineStatusGrain : Grain, IUserOnlineStatusGrain
   {
+    public UserOnlineStatusGrain(ILogger<UserOnlineStatusGrain> logger)
+    {
+      this.logger = logger;
+    }
+
+    private readonly ILogger<UserOnlineStatusGrain> logger;
+
     private IDisposable offlineNotificationTimer;
 
     private HashSet<string> clients = new HashSet<string>();
@@ -25,12 +33,12 @@ namespace Yotalab.PlanningPoker.Grains
       var isRemoved = this.clients.Remove(clientId);
       if (this.clients.Count == 0 && isRemoved)
       {
-        this.offlineNotificationTimer?.Dispose();
+        this.DisposeOfflineNotificationTimer();
 
         // Делаем небольшую задержку, чтобы исключить ситуации быстрого обновления статуса.
         // Например, если пользователь обновил страницу, то без задержки статус успеет моргнуть.
         this.offlineNotificationTimer = this.RegisterTimer(
-          this.OnNotifyOfflineTimer, null, TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(500));
+          this.OnNotifyOfflineTimer, null, TimeSpan.FromMilliseconds(500), TimeSpan.MaxValue);
       }
 
       return Task.CompletedTask;
@@ -42,9 +50,13 @@ namespace Yotalab.PlanningPoker.Grains
       if (this.clients.Count == 1 && isAdded)
       {
         // Если пользователь успел появиться в сети, то не нужно уведомлять об выходе из сети.
-        this.offlineNotificationTimer?.Dispose();
+        var offlineNotificationTimerActive = this.offlineNotificationTimer != null;
+        this.DisposeOfflineNotificationTimer();
 
         this.NotifyOnlineStatusChanged();
+
+        if (!offlineNotificationTimerActive)
+          this.logger.LogInformation($"User {this.GetPrimaryKey()} online");
       }
 
       return Task.CompletedTask;
@@ -52,7 +64,10 @@ namespace Yotalab.PlanningPoker.Grains
 
     private Task OnNotifyOfflineTimer(object state)
     {
+      this.DisposeOfflineNotificationTimer();
+
       this.NotifyOnlineStatusChanged();
+      this.logger.LogInformation($"User {this.GetPrimaryKey()} offline");
       return Task.CompletedTask;
     }
 
@@ -62,6 +77,12 @@ namespace Yotalab.PlanningPoker.Grains
       this.GetStreamProvider("SMS").GetStream<UserOnlineStatusChanged>(userId, typeof(UserOnlineStatusChanged).FullName)
         .OnNextAsync(new UserOnlineStatusChanged(userId, this.HasActiveClients))
         .Ignore();
+    }
+
+    private void DisposeOfflineNotificationTimer()
+    {
+      this.offlineNotificationTimer?.Dispose();
+      this.offlineNotificationTimer = null;
     }
   }
 }
